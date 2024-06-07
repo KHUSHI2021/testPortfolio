6d92c8fe1743c16f92e5175fea0bce73b209136b
khushi <110249789+KHUSHI2021@users.noreply.github.com>
2024-06-07 06:33:40 +0000

    hey

--- 01_basic/datatype.js
+++ 01_basic/datatype.js
@@ -1,3 +1,8 @@
  "use strict";  //treat all js code as newer version
 //  alert(9+9)  // we using node.js, not browser
 
+let string="khushi"
+let number=5
+let accountId;
+console.log(typeof undefined); //undefind
+console.log(typeof null); //object
