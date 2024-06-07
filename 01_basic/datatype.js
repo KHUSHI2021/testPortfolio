fce572d1e9c061c42d4ce55de9e3411d4b23c864
khushi <110249789+KHUSHI2021@users.noreply.github.com>
2024-06-07 06:02:11 +0000

    starting

---
+++ 01_basic/datatype.js
@@ -0,0 +1,3 @@
+ "use strict";  //treat all js code as newer version
+//  alert(9+9)  // we using node.js, not browser
+
---
+++ 01_basic/var.js
@@ -0,0 +1,11 @@
+const accountId= 2493849
+let accountEmail="khushi.bt.21@nitj.ac.in"
+console.log(accountEmail);
+console.log(accountId);
+ 
+accountEmail="khushi.gupta110203@gmial.com"
+console.log(accountEmail);
+console.table([accountEmail,accountId])
+
+/* prefer not use of var
+bcz of issue in blockand func scope */
