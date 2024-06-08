b371aa8acc26b303e70ed7ea5c482420c70da560
khushi <110249789+KHUSHI2021@users.noreply.github.com>
2024-06-08 12:08:05 +0000

    obj

---
+++ 01_basic/obj.js
@@ -0,0 +1,13 @@
+//singleton
+// obj literals
+ 
+const object={
+    email:"khushi.gupta11023@gmail.com",
+    age:21,
+    "name":"khushi",
+    rollNumber:21111026,
+}
+
+console.log(object.age)
+console.log(object["rollNumber"])//more using methode bcz in symbole, any "name"
+console.log(object["name"])
